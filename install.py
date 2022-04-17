b0091a9135b55a43529eb81d8159559a80b557b2
Honza0105 <honzajelinek@email.cz>
2022-04-17 19:35:45 +0200

    Addition of the new password

--- install.py
+++ install.py
@@ -6,7 +6,7 @@ def log_in(database_name="testdatabase"): #log into the database
         db = mysql.connector.connect(
             host="localhost",
             user="root",
-            passwd=os.environ.get("DBPassword"),
+            passwd="MyN3wP4ssw0rd!*",
             database=database_name)
 
         print("database has already been installed")
@@ -15,7 +15,7 @@ def log_in(database_name="testdatabase"): #log into the database
         db = mysql.connector.connect(
             host="localhost",
             user="root",
-            passwd=os.environ.get("DBPassword"))
+            passwd="MyN3wP4ssw0rd!*")
 
         mycursor = db.cursor()
         mycursor.execute(f"create database {database_name}")
@@ -23,15 +23,16 @@ def log_in(database_name="testdatabase"): #log into the database
 
 def initialization(my_cursor):
     #here we are going to build the database's core
+    '''
     my_cursor.execute("CREATE USER 'user'@'localhost' IDENTIFIED BY 'Password123_'")
     my_cursor.execute("FLUSH PRIVILEGES;")
-
+    '''
 
 
 if __name__ == "__main__":
-    db = log_in()
+    db = log_in("employees")
     mycursor = db.cursor()
-
+    print(mycursor)
     initialization(mycursor)
 
     #here we could reference the starting app Ig
