fd34664e624abf3543b06c7b09d560e7e106b960
vortex552 <nielsvwinden@gmail.com>
2022-05-11 11:48:06 +0200

    created employee and admin class

--- AdminClass.py
+++ AdminClass.py
@@ -1,1 +1,13 @@
+class Admin:
+    def search(ISBN):
 
+
+class Employee:
+    def search(book_id):
+        return f"select a.title, b.author, a.edition, a.location, a.section, a.language, d.sellprice from book a, author b, book_entry c, Price_exceptions d where a.ISBN=b.ISBN and a.ISBN=c.ISBN and book_id='{book_id}'"
+
+Niels = Employee
+
+query = Niels.search(1234)
+
+print(query)
