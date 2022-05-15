a6853bb0e6d5b469f556f3d50518902cebf15a7b
Honza0105 <honzajelinek@email.cz>
2022-05-15 09:43:34 +0200

    update amorlibrorum - it shoudl be up to date, I've forgotten to commit it, sry

--- AmorLibrorum.sql
+++ AmorLibrorum.sql
@@ -1,46 +1,69 @@
-CREATE TABLE `Book` (
-  `ISBN` varchar(17) NOT NULL,
+CREATE TABLE `Books` (
+  `ISBN` varchar(13) NOT NULL,
   `Title` varchar(50) NOT NULL,
-  `Title_untranslated` varchar(50),
-  `Author(s)` varchar(100) NOT NULL,
   `publisher` varchar(20) NOT NULL,
-  `published(year)` int(4) NOT NULL,
-  `pages` int(5) NOT NULL,
+  `published(year)` int NOT NULL,
+  `pages` int NOT NULL,
   `language` varchar(30) NOT NULL,
-  `translator` varchar(30),
-  `edition` int(2) ,
+  `edition` int,
   `book_type` ENUM("paperback","hardcover"),
-  `location` int(3) NOT NULL,
-  `section` int(3) NOT NULL,
+  `location` int NOT NULL,
+  `section` int NOT NULL,
+  `Genre` varchar(30),
   PRIMARY KEY (`ISBN`)
 );
 
-CREATE TABLE `Book_entry` (
-  `Book_ID` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `Book_entries` (
+  `Book_ID` int NOT NULL AUTO_INCREMENT,
   `copy_of(ISBN)` varchar(17) NOT NULL,
-  `status` ENUM("used","just like new","new") DEFAULT "new",
+  `status/comment` varchar(100),
   PRIMARY KEY (`Book_ID`),
-  FOREIGN KEY (`copy_of(ISBN)`) REFERENCES `Book`(`ISBN`)
+  FOREIGN KEY (`copy_of(ISBN)`) REFERENCES `Books`(`ISBN`)
+);
+
+CREATE TABLE `Price_exceptions` (
+  `Book_ID` int NOT NULL,
+  `New_price(in_cents)` int,
+  `Comment` varchar(30),
+  FOREIGN KEY (`Book_ID`) REFERENCES `Book_entries`(`Book_ID`)
 );
 
 CREATE TABLE `Employees` (
-  `Employee_ID` int(4) NOT NULL AUTO_INCREMENT,
+  `Employee_ID` int NOT NULL AUTO_INCREMENT,
   `Name` varchar(50) NOT NULL,
   `Surname` varchar(50) NOT NULL,
   `position` ENUM("Staff","Manager") NOT NULL,
   `Password` varchar(50) NOT NULL,
+  `email` varchar(100),
   PRIMARY KEY (`Employee_ID`)
 );
 
 CREATE TABLE `Transactions` (
-  `Transaction_ID` int(11) NOT NULL AUTO_INCREMENT,
-  `Book_ID(local)` int(11) NOT NULL,
-  `Employee_ID` int(4) NOT NULL,
+  `Transaction_ID` int NOT NULL AUTO_INCREMENT,
+  `Book_ID(local)` int NOT NULL,
+  `Employee_ID` int NOT NULL,
   `Date` DATE NOT NULL,
-  `Price` int(4) NOT NULL,
-  `Type` ENUM("Buy","Sell") NOT NULL,
+  `Price(in_cents)` int NOT NULL,
   PRIMARY KEY (`Transaction_ID`),
-  FOREIGN KEY (`Book_ID(local)`) REFERENCES `Book_entry`(`Book_ID`),
-  FOREIGN KEY (`Employee_ID`) REFERENCES `Employees`(`Employee_ID`)
+  FOREIGN KEY (`Employee_ID`) REFERENCES `Employees`(`Employee_ID`),
+  FOREIGN KEY (`Book_ID(local)`) REFERENCES `Book_entries`(`Book_ID`)
+);
+
+CREATE TABLE `if_translated` (
+  `ISBN` varchar(13) NOT NULL,
+  `translator` varchar(50) NOT NULL,
+  `Title_untranslated` varchar(50),
+  `translated_from` varchar(20) NOT NULL,
+  FOREIGN KEY (`ISBN`) REFERENCES `Books`(`ISBN`)
+);
+
+CREATE TABLE `variables` (
+  `margin` int
+);
+
+CREATE TABLE `Authors` (
+  `author_id` int NOT NULL,
+  `author_name` varchar(50),
+  `author_surname` varchar(50)
 );
 
