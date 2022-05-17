435fe6709e818f577491d142eb220a24a9a403a7
Honza0105 <honzajelinek@email.cz>
2022-05-17 10:39:46 +0200

    update connect and admin class

--- AdminClass.py
+++ AdminClass.py
@@ -1,10 +1,14 @@
 import connect.py
 
 class Admin:
+    def __init__(self, password):
+        connection = connect.connect_admin(password)
     def search(ISBN):
 
 
 class Employee:
+    def __init__(self):
+        connection = connect.connect_user()
     def search(book_id):
         return f"select a.title, b.author, a.edition, a.location, a.section, a.language, d.sellprice from book a, author b, book_entry c, Price_exceptions d where a.ISBN=b.ISBN and a.ISBN=c.ISBN and book_id='{book_id}'"
 
