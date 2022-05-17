8702660fbcf3deaa026f43262e055a16bede039a
Honza0105 <honzajelinek@email.cz>
2022-05-17 10:47:40 +0200

    create employee function
    not finished - will need to add them more rights

--- install.py
+++ install.py
@@ -39,6 +39,18 @@ def user_intergration(my_cursor):
     for i in mycursor:
         print(i[0])
 
+def employee_account_addition(my_cursor,username,password):
+    try:
+        my_cursor.execute(f"CREATE USER '{username}'@'localhost' IDENTIFIED BY '{password}'")
+        my_cursor.execute("FLUSH PRIVILEGES;")
+        my_cursor.execute("GRANT SELECT ON AmorLibrorum.book TO 'user'@'localhost'")
+        ##Give more power eventually
+    except mysql.connector.errors.DatabaseError:
+        mycursor.execute(f"DROP USER '{username}'@'localhost';")
+        return user_intergration(my_cursor)
+    mycursor.execute("show tables;")
+    for i in mycursor:
+        print(i[0])
 
 if __name__ == "__main__":
     db = log_in()
