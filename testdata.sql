f4cc87f2634032b7396424c4c23d42cd9f93fdba
gabrielius_d <94465193+gabrielius-d@users.noreply.github.com>
2022-05-17 12:10:40 +0200

    Update testdata.sql

--- testdata.sql
+++ testdata.sql
@@ -4,7 +4,7 @@ LOCK TABLES `books` WRITE;
 /*!40000 ALTER TABLE `books` DISABLE KEYS */;
 INSERT INTO `books` VALUES (9781501110368, "It Ends with Us","Atria","2016", "384", "English", null, "paperback", "78", "58", "College Romance, Fiction");
 INSERT INTO `books` VALUES (9780679805274, "Oh, the Places You'll Go!","Random House Books for Young Readers","1990", "56", "English", null, null, "77", "78", "Children Classics");
-INSERT INTO `books` VALUES (9781476797090, "Phil: The Rip-Roaring (and Unauthorized!) Biography of Golf's Most Colorful Superstar","Avid Reader Press / Simon & Schuster","2022", "256", "English", null, hardcover, "747", "178", "Biographies");
+INSERT INTO `books` VALUES (9781476797090, "Phil: The Rip-Roaring (and Unauthorized!) Biography of Golf's Most Colorful Superstar","Avid Reader Press / Simon & Schuster","2022", "256", "English", null, "hardcover", "747", "178", "Biographies");
 INSERT INTO `books` VALUES (9780735211292, "Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones","Avery","2018", "320", "English", null, "hardcover", "11", "7", "Psychology");
 INSERT INTO `books` VALUES (9781538724731, "Verity","Grand Central Publishing","2021", "336", "English", null, "paperback", "74", "578", "Suspense, Thriller");
 INSERT INTO `books` VALUES (9781501161938, "The Seven Husbands of Evelyn Hugo: A Novel","Washington Square Press","2018", "400", "English", null, "paperback", "2", "718", "Holiday Romance");
