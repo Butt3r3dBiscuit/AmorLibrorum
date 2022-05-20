b7fdf35cf793efdb23899a859d89158c6b9a45da
Honza0105 <honzajelinek@email.cz>
2022-05-20 16:53:41 +0200

    fix - forgot to make margin float

--- functions_in_sql.sql
+++ functions_in_sql.sql
@@ -1,17 +1,18 @@
 DELIMITER $$
+drop function if exists price_determination;
 
 CREATE function price_determination(book_id_given int)
     returns int
     Deterministic
     begin
         declare price int;
-        declare margin_var int;
+        declare margin_var float;
         declare buying_price int;
         select new_price_in_cents into price from Price_exceptions where Book_ID=book_id_given;
         if price IS NULL THEN
             select margin into margin_var from variables;
             select Price_in_cents into buying_price from Transactions where Book_ID=book_id_given;
-            SET price = - buying_price * margin_var;
+            SET price = -buying_price * margin_var;
         end if;
     RETURN price;
 END$$
