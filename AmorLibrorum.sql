53fab69af77726afe9d37d0120371063115d0600
gabrielius_d <94465193+gabrielius-d@users.noreply.github.com>
2022-05-20 12:53:04 +0200

    Update AmorLibrorum.sql

--- AmorLibrorum.sql
+++ AmorLibrorum.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `Books` (
   `ISBN` varchar(13) NOT NULL,
-  `Title` varchar(50) NOT NULL,
-  `publisher` varchar(20) NOT NULL,
+  `Title` varchar(100) NOT NULL,
+  `publisher` varchar(100) NOT NULL,
   `year_published` int NOT NULL,
   `pages` int NOT NULL,
   `language` varchar(30) NOT NULL,
@@ -9,13 +9,13 @@ CREATE TABLE `Books` (
   `book_type` ENUM("paperback","hardcover"),
   `location` int NOT NULL,
   `section` int NOT NULL,
-  `Genre` varchar(30),
+  `Genre` varchar(50),
   PRIMARY KEY (`ISBN`)
 );
 
 CREATE TABLE `Book_entries` (
   `Book_ID` int NOT NULL AUTO_INCREMENT,
-  `ISBN` varchar(17) NOT NULL,
+  `ISBN` varchar(13) NOT NULL,
   `status/comment` varchar(100),
   PRIMARY KEY (`Book_ID`),
   FOREIGN KEY (`ISBN`) REFERENCES `Books`(`ISBN`)
@@ -24,7 +24,7 @@ CREATE TABLE `Book_entries` (
 CREATE TABLE `Price_exceptions` (
   `Book_ID` int NOT NULL,
   `new_price_in_cents` int,
-  `comment` varchar(30),
+  `Comment` varchar(30),
   FOREIGN KEY (`Book_ID`) REFERENCES `Book_entries`(`Book_ID`)
 );
 
@@ -37,7 +37,7 @@ CREATE TABLE `if_translated` (
 );
 
 CREATE TABLE `Authors` (
-  `ISBN` int NOT NULL,
+  `ISBN` varchar(13) NOT NULL,
   `author_name` varchar(50),
   `author_surname` varchar(50)
 );
@@ -48,19 +48,19 @@ CREATE TABLE `Employees` (
   `Surname` varchar(50) NOT NULL,
   `position` ENUM("Staff","Manager") NOT NULL,
   `Password` varchar(50) NOT NULL,
-  `email` varchar(100),
+  `email` varchar(100) NOT NULL,
   PRIMARY KEY (`Employee_ID`)
 );
 
 CREATE TABLE `Transactions` (
   `Transaction_ID` int NOT NULL AUTO_INCREMENT,
-  `Book_ID(local)` int NOT NULL,
+  `Book_ID` int NOT NULL,
   `Employee_ID` int NOT NULL,
   `Date` DATE NOT NULL,
-  `price_in_cents` int NOT NULL,
+  `Price_in_cents` int NOT NULL,
   PRIMARY KEY (`Transaction_ID`),
   FOREIGN KEY (`Employee_ID`) REFERENCES `Employees`(`Employee_ID`),
-  FOREIGN KEY (`Book_ID(local)`) REFERENCES `Book_entries`(`Book_ID`)
+  FOREIGN KEY (`Book_ID`) REFERENCES `Book_entries`(`Book_ID`)
 );
 
 CREATE TABLE `variables` (
