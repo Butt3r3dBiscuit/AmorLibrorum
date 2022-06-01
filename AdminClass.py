0c417b5d91d66f7cb21fac191d1f040063b4489e
vortex552 <nielsvwinden@gmail.com>
2022-06-01 21:31:04 +0200

    add book procedure

--- AdminClass.py
+++ AdminClass.py
@@ -1,98 +1,45 @@
 import connect
+from user_creation import employee_user_addition
 
-
-# insert functions
-
-def add_to_books(ISBN, Title, publisher, published_year, pages, language, edition, book_type, location, section, genre):
-    query = f"insert into books values ({ISBN}, {Title}, {publisher}, {published_year}, {pages}, {language}, " \
-            f"{edition}, {book_type}, {location}, {section}, {genre})"
+# insert queries
+def add_to_books(self, ISBN, Title, publisher, published_year, pages, language, edition, book_type, location, section, genre):
+    query = f"insert into books values ({ISBN}, {Title}, {publisher}, {published_year}, {pages}, {language}," \
+            f" {edition}, {book_type}, {location}, {section}, {genre})"
     return query
 
-
-def add_to_authors(ISBN, author_name, author_surname):
+def add_to_authors(self, ISBN, author_name, author_surname):
     query = f"insert into Authors values ({ISBN}, {author_name}, {author_surname})"
     return query
 
-
-def add_to_if_translated(ISBN, translator, Title_translated, translated_from):
+def add_to_if_translated(self, ISBN, translator, Title_translated, translated_from):
     query = f"insert into if_translated values ({ISBN}, {translator}, {Title_translated}, {translated_from})"
     return query
 
-
-def add_to_Book_entries(ISBN, status_comment):
+def add_to_Book_entries(self, ISBN, status_comment):
     query = f"insert into book_entries(ISBN, status/comment) values ({ISBN}, {status_comment})"
     return query
 
-
-def add_to_Price_exceptions(book_id, newprice, comment):
+def add_to_Price_exceptions(self, book_id, newprice, comment):
     query = f"insert into Price_exceptions values ({book_id}, {newprice}, {comment})"
     return query
 
-
-def add_to_Transactions(book_id, employee_id, date, Price):
-    query = f"insert into transactions(book_ID, Employee_ID, date, Price_in_cents) values ({book_id}, {employee_id}," \
-            f" {date}, {Price})"
+def add_to_Transactions(self, book_id, employee_id, date, Price):
+    query = f"insert into transactions(Book_ID, Employee_ID, Date, Price_in_cents) values ({book_id}," \
+            f" {employee_id}, {date}, {Price})"
     return query
 
-
-def add_to_employees(Name, Surname, position, Password, email):
-    query = f"insert into employees(Name, Surname, position, Password, email) values({Name}, {Surname}, {position}," \
-            f" {Password}, {email})"
+def add_to_employees(self, Name, Surname, position, Password, email):
+    query = f"insert into employees(Name, Surname, position, Password, email) values({Name}, {Surname}," \
+            f" {position}, {Password}, {email})"
     return query
 
-
-def add_to_variables(margin):
+def add_to_variables(self, margin):
     query = f"insert into variables(margin) values ({margin})"
-    return query
-
-
-# admin search
-def inventory_search(ISBN):
-    ISBN = 9780593334833
-    query = f"select a.ISBN, a.title, b.author_name, b.author_surname, a.publisher, a.year_published, a.pages," \
-            f" a.language, a.edition, a.book_type, a.location, a.section, a.Genre from books a," \
-            f" authors b where a.ISBN={ISBN} and b.ISBN={ISBN};"
     return query
 
 
 # Admin class query
 class Admin:
-    # insert queries
-    def add_to_books(self, ISBN, Title, publisher, published_year, pages, language, edition, book_type, location, section, genre):
-        query = f"insert into books values ({ISBN}, {Title}, {publisher}, {published_year}, {pages}, {language}," \
-                f" {edition}, {book_type}, {location}, {section}, {genre})"
-        return query
-
-    def add_to_authors(self, ISBN, author_name, author_surname):
-        query = f"insert into Authors values ({ISBN}, {author_name}, {author_surname})"
-        return query
-
-    def add_to_if_translated(self, ISBN, translator, Title_translated, translated_from):
-        query = f"insert into if_translated values ({ISBN}, {translator}, {Title_translated}, {translated_from})"
-        return query
-
-    def add_to_Book_entries(self, ISBN, status_comment):
-        query = f"insert into book_entries(ISBN, status/comment) values ({ISBN}, {status_comment})"
-        return query
-
-    def add_to_Price_exceptions(self, book_id, newprice, comment):
-        query = f"insert into Price_exceptions values ({book_id}, {newprice}, {comment})"
-        return query
-
-    def add_to_Transactions(self, book_id, employee_id, date, Price):
-        query = f"insert into transactions(Book_ID, Employee_ID, Date, Price_in_cents) values ({book_id}," \
-                f" {employee_id}, {date}, {Price})"
-        return query
-
-    def add_to_employees(self, Name, Surname, position, Password, email):
-        query = f"insert into employees(Name, Surname, position, Password, email) values({Name}, {Surname}," \
-                f" {position}, {Password}, {email})"
-        return query
-
-    def add_to_variables(self, margin):
-        query = f"insert into variables(margin) values ({margin})"
-        return query
-
     # inventory search
     def inventory_search_authors_books(self, ISBN=9780593334833):  #
         #returns ISBN, title, author_name, author_surname, publisher, year_published,
@@ -165,11 +112,47 @@ class Admin:
             else:
                 query += f" and a.date<'{end_date}'"
         return query
+    
+    # employee search
+    def employee_search(self, name, surname):
+        #returns employee_id, name, surname,  email, position
+        query = f"select a.employee_id, a.name, a.surname, a.email, a.position from employees a where a.Name='{name}' and a.Surname='{surname}'"
+        return query
+    
+    # procedures
+    def add_user(self, mycursor, email, password):
+        employee_user_addition(mycursor, email, password)
+
+    def add_book(self, mycursor, ISBN, Title, author_name, author_surname, publisher, published_year, pages, language, book_type, location, section, genre, employee_id, date, Price, status_comment=None,translator=None, Title_translated=None, translated_from=None, edition=None, number_of_copies=1):
+        #adds to book_entries
+        for i in range(number_of_copies):
+            mycursor.execute(add_to_Book_entries(ISBN, status_comment))
+        #if book not yet in database adds to books, auhtors, transactions, if_translated
+        ISBN_list = mycursor.execute("select ISBN from books")
+        for i in ISBN_list:
+            if ISBN in i:
+                #adds to books
+                mycursor.execute(add_to_books(ISBN, Title, publisher, published_year, pages, language, edition, book_type, location, section, genre))
+                #adds to authors
+                mycursor.execute(add_to_authors(ISBN, author_name, author_surname))
+                #adds to transactions
+                book_id_list = mycursor.execute(f"select book_id from book_entries where ISBN={ISBN}")
+                for x in book_id_list:
+                    for book_id in x:
+                        mycursor.execute(add_to_Transactions(book_id, employee_id, date, Price))
+                #adds to translated if translated
+                if translator!=None or Title_translated!=None or translated_from!=None:
+                    mycursor.execute(add_to_if_translated(ISBN, translator, Title_translated, translated_from))
+
+
+
 
 
 admin = Admin()
-curry = admin.profits(ISBN=9780593334833, employee_id=2)
-print(curry)
+db = connect.connect_admin("MyN3wP4ssw0rd!*")
+mycursor= db.cursor()
+
+admin.add_book(mycursor=mycursor, ISBN, Title, author_name, author_surname, publisher, published_year, pages, language, book_type, location, section, genre, employee_id, date, Price, status_comment=None,translator=None, Title_translated=None, translated_from=None, edition=None, number_of_copies=1)
 
 
 class Employee:
@@ -187,12 +170,12 @@ class Employee:
 
 db = connect.connect_admin("MyN3wP4ssw0rd!*") #temporal solution
 
-def query_to_values(db, query):  # returns a list of values
-    mycursor = db.cursor()
-    mycursor.execute(query)
-    for (x) in mycursor:
-        return x
+# def query_to_values(db, query):  # returns a list of values
+#     mycursor = db.cursor()
+#     mycursor.execute(query)
+#     for (x) in mycursor:
+#         return x
 
 
-test = (query_to_values(db, curry))
-print(test)
+# test = (query_to_values(db, curry))
+# print(test)
