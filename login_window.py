52a0377cc416e315fc3b99bdcfd07e78467f4cad
Honza0105 <honzajelinek@email.cz>
2022-06-03 10:18:34 +0200

    now we get input from email field

--- login_window.py
+++ login_window.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import Start_window
+from connect import connect_admin, connect_employee
 
 class login_window(tk.Frame):
     def __init__(self, parent, controller):
@@ -21,19 +22,24 @@ class login_window(tk.Frame):
         back_button.place(x=0,y=0)
 
 
-        login_button = tk.Button(self, text="Log in", height=2, width=11)
+        login_button = tk.Button(self, text="Log in", height=2, width=11, command= self.log_in)
         login_button.pack()
         login_button.place(x=200, y=95)
 
         email_label = tk.Label(self, text="Email Adress", width = "15")
         email_label.pack()
-        email_text = tk.Text(self, height=1, width=30)
-        email_text.pack()
+        self.email_text = tk.Text(self, height=1, width=30)
+        self.email_text.pack()
 
         password_label = tk.Label(self, text="Password", width = "15")
         password_label.pack()
         password_text = tk.Text(self, height=1, width=30)
         password_text.pack()
+
+    def log_in(self):
+        email = self.email_text.get("1.0", "end")
+        print(email)
+
 
         '''
          except Exception as e:
