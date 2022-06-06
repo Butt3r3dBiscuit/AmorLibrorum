624c9d0882006a1a292d411d29f3c70d193794ec
vortex552 <nielsvwinden@gmail.com>
2022-06-06 03:14:06 +0200

    search records method

--- AdminClass.py
+++ AdminClass.py
@@ -39,7 +39,66 @@ def add_to_variables(margin):
     query = f"insert into variables(margin) values ({margin})"
     return query
 
+# transaction search
+def func_search_records(ISBN=None, Employee_id=None, Book_id=None):
+    #returns book_id, ISBN, title, author_name, author_surname, edition, book_type, name, surname, price_in_cents
+    query = f"select c.book_id, a.ISBN, a.title, b.author_name, b.author_surname, a.edition," \
+            f" a.book_type, e.name, e.surname, c.price_in_cents from books a, authors b, transactions c, book_entries d," \
+            f" employees e where a.ISBN=d.ISBN and c.book_id=d.book_id and d.ISBN=b.ISBN and" \
+            f" e.employee_id=c.employee_id"
+    if ISBN!=None:
+        query+= f" and a.ISBN={ISBN}"
+    if Employee_id!=None:
+        query+= f" and e.Employee_id={Employee_id}"
+    if Book_id!=None:
+        query+= f" and c.book_id={Book_id}"
+    return query
 
+def num_of_sales(self, start_date=None, end_date=None, Employee_id=None, ISBN=None):
+    #returns number of sales
+    query = f"select count(a.book_id) from transactions a, book_entries b" \
+            f" where a.book_id=b.book_id and a.price_in_cents>0"
+    if ISBN is not None:
+        query = query + f" and b.ISBN={ISBN}"
+    if Employee_id is not None:
+        query += f" and a.Employee_id={Employee_id}"
+    if start_date is not None:
+        query += f" and a.date>={start_date}"
+    if end_date is not None:
+        query += f" and a.date<{end_date}"
+    return query
+
+def profits(self, ISBN=None, employee_id=None, start_date=None, end_date=None):
+    #returns profit in cents
+    query = f"select sum(a.price_in_cents) from transactions a, book_entries b"
+    if ISBN is not None:
+        if "=" not in query:
+            query += f" where a.book_id=b.book_id and b.ISBN={ISBN}"
+        else:
+            query += f" and a.book_id=b.book_id and b.ISBN={ISBN}"
+    if employee_id is not None:
+        if "=" not in query:
+            query += f" where a.employee_id={employee_id}"
+        else:
+            query += f" and a.employee_id={employee_id}"
+    if start_date is not None:
+        if ("=" or ">=") not in query:
+            query += f" where a.date>='{start_date}'"
+        else:
+            query += f" and a.date>='{start_date}'"
+    if end_date is not None:
+        if ("=" or "<") not in query:
+            query += f" where a.date<'{end_date}'"
+        else:
+            query += f" and a.date<'{end_date}'"
+    return query
+    
+# employee search
+def employee_search(name, surname):
+    #returns employee_id, name, surname,  email, position
+    query = f"select a.employee_id, a.name, a.surname, a.email, a.position from employees a where a.Name='{name}' and a.Surname='{surname}'"
+    return query
+    
 # Admin class query
 class Admin:
     # inventory search
@@ -60,87 +119,33 @@ class Admin:
         #returns list of book_ids
         query = f"select a.book_id from book_entries a, price_exceptions b where a.book_id=b.book_id and a.ISBN={ISBN}"
         return query
-
-    # transaction search
-    def search_records(self, ISBN=None, Employee_id=None, Book_id=None):
-        #returns book_id, ISBN, title, author_name, author_surname, edition, book_type, name, surname, price_in_cents
-        query = f"select c.book_id, a.ISBN, a.title, b.author_name, b.author_surname, a.edition," \
-                f" a.book_type, e.name, e.surname, c.price_in_cents from books a, authors b, transactions c, book_entries d," \
-                f" employees e where a.ISBN=d.ISBN and c.book_id=d.book_id and d.ISBN=b.ISBN and" \
-                f" e.employee_id=c.employee_id"
-        if ISBN!=None:
-            query+= f" and a.ISBN={ISBN}"
-        if Employee_id!=None:
-            query+= f" and e.Employee_id={Employee_id}"
-        if Book_id!=None:
-            query+= f" and c.book_id={Book_id}"
-        return query
-
-    def num_of_sales(self, start_date=None, end_date=None, Employee_id=None, ISBN=None):
-        #returns number of sales
-        query = f"select count(a.book_id) from transactions a, book_entries b" \
-                f" where a.book_id=b.book_id and a.price_in_cents>0"
-        if ISBN is not None:
-            query = query + f" and b.ISBN={ISBN}"
-        if Employee_id is not None:
-            query += f" and a.Employee_id={Employee_id}"
-        if start_date is not None:
-            query += f" and a.date>={start_date}"
-        if end_date is not None:
-            query += f" and a.date<{end_date}"
-        return query
-
-    def profits(self, ISBN=None, employee_id=None, start_date=None, end_date=None):
-        #returns profit in cents
-        query = f"select sum(a.price_in_cents) from transactions a, book_entries b"
-        if ISBN is not None:
-            if "=" not in query:
-                query += f" where a.book_id=b.book_id and b.ISBN={ISBN}"
-            else:
-                query += f" and a.book_id=b.book_id and b.ISBN={ISBN}"
-        if employee_id is not None:
-            if "=" not in query:
-                query += f" where a.employee_id={employee_id}"
-            else:
-                query += f" and a.employee_id={employee_id}"
-        if start_date is not None:
-            if ("=" or ">=") not in query:
-                query += f" where a.date>='{start_date}'"
-            else:
-                query += f" and a.date>='{start_date}'"
-        if end_date is not None:
-            if ("=" or "<") not in query:
-                query += f" where a.date<'{end_date}'"
-            else:
-                query += f" and a.date<'{end_date}'"
-        return query
     
-    # employee search
-    def employee_search(self, name, surname):
-        #returns employee_id, name, surname,  email, position
-        query = f"select a.employee_id, a.name, a.surname, a.email, a.position from employees a where a.Name='{name}' and a.Surname='{surname}'"
-        return query
-    
-    # procedures
+    #procedures
     def add_user(self, mycursor, email, password):
         employee_user_addition(mycursor, email, password)
 
     def add_book(self, mycursor, ISBN, Title, author_name, author_surname, publisher, published_year, pages, language, book_type, location, section, genre, employee_id, date, Price, status_comment="NULL", translator="NULL", Title_untranslated="NULL", translated_from="NULL", edition="NULL", number_of_copies=1):
-        #queries
-        add_book_entries = add_to_Book_entries(ISBN=ISBN, status_comment=status_comment)
-        print(add_book_entries)
-        add_book = add_to_books(ISBN=ISBN, Title=Title, publisher=publisher, published_year=published_year, pages=pages, language=language, edition=edition, book_type=book_type, location=location, section=section, genre=genre)
-        print(add_book)
-        add_authors = add_to_authors(ISBN=ISBN, author_name=author_name, author_surname=author_surname)
-        print(add_authors)
-        add_if_translated = add_to_if_translated(ISBN, translator, Title_untranslated, translated_from)
-        print(add_if_translated)
-
-        #if book not yet in database adds to books, auhtors, if_translated
-        ISBN_list = mycursor.execute("select ISBN from books", multi=True)
-        print(ISBN_list)
-        for i in ISBN_list:
-            if ISBN not in i:
+        try:
+            #queries
+            add_book_entries = add_to_Book_entries(ISBN=ISBN, status_comment=status_comment)
+            print(add_book_entries)
+            add_book = add_to_books(ISBN=ISBN, Title=Title, publisher=publisher, published_year=published_year, pages=pages, language=language, edition=edition, book_type=book_type, location=location, section=section, genre=genre)
+            print(add_book)
+            add_authors = add_to_authors(ISBN=ISBN, author_name=author_name, author_surname=author_surname)
+            print(add_authors)
+            add_if_translated = add_to_if_translated(ISBN, translator, Title_untranslated, translated_from)
+            print(add_if_translated)
+            #checks if price is negative
+            if Price<0:
+                Price*=-1
+            #if book not yet in database adds to books, auhtors, if_translated
+            mycursor.execute("select ISBN from books")
+            ISBN_list_fetch = mycursor.fetchall()
+            ISBN_list = []
+            for i in ISBN_list_fetch:
+                for j in i:
+                    ISBN_list.append(j)
+            if ISBN not in ISBN_list:
                 #adds to books
                 mycursor.execute(add_book)
                 #adds to authors
@@ -148,35 +153,46 @@ class Admin:
                 #adds to translated if translated
                 if translator!=None or Title_untranslated!=None or translated_from!=None:
                     mycursor.execute(add_if_translated)
+            #adds to book_entries
+            for i in range(number_of_copies):
+                mycursor.execute(add_book_entries)
         
-        #adds to book_entries
-        for i in range(number_of_copies):
-            mycursor.execute(add_book_entries)
+            #adds to transactions
+            mycursor.execute(f"select book_id from book_entries where ISBN={ISBN}")
+            book_id_list_fetch = mycursor.fetchall()
+            book_id_list = []
+            for i in book_id_list_fetch:
+                for j in i:
+                    book_id_list.append(j)
+            for i in book_id_list:
+                print(add_to_Transactions(book_id=i, employee_id=employee_id, date=date, Price=Price))
+                mycursor.execute(add_to_Transactions(book_id=i, employee_id=employee_id, date=date, Price=Price))
+        except mysql.connector.errors.IntegrityError:
+            print(f"Error: 1062 (23000): Duplicate entry '{ISBN}' for key 'books.PRIMARY'")
+    
+    def search_records(self, mycursor, ISBN=None, Employee_id=None, Book_id=None):
+        search = func_search_records(ISBN=ISBN, Employee_id=Employee_id, Book_id=Book_id)
+        print(search)
+        mycursor.execute(search)
+        result_fetch = mycursor.fetchall()
+        return result_fetch
         
-        #adds to transactions
-        book_id_list = mycursor.execute(f"select book_id from book_entries where ISBN={ISBN}", multi=True)
-        for l1st in book_id_list:
-            for book_id in l1st:
-                print(add_to_Transactions(book_id=book_id[0], employee_id=employee_id, date=date, Price=Price))
-                mycursor.execute(add_to_Transactions(book_id=book_id[0], employee_id=employee_id, date=date, Price=Price))
-
 admin = Admin()
 db = connect.connect_admin("MyN3wP4ssw0rd!*")
 mycursor= db.cursor()
+mycursor.execute("select ISBN from books")
+ISBN_list = mycursor.fetchall()
 
 #call add_book procedure
-admin.add_book(mycursor=mycursor, ISBN=9780590353403, Title="Harry Potter and the Sorcerers Stone", author_name="Joanne", author_surname="Rowling", publisher="Scholastic Inc", published_year=2003, pages="309", language="English (USA)", book_type="Hardcover", location="7", section="7", genre="Fiction", employee_id=2, date="2022-06-02", Price=1000, translator="Joanne Rowling", Title_untranslated="Harry Potter and the Philosophers Stone", translated_from="English", edition=1, number_of_copies=1)
+#admin.add_book(mycursor=mycursor, ISBN=9780590353403, Title="Harry Potter and the Sorcerers Stone", author_name="Joanne", author_surname="Rowling", publisher="Scholastic Inc", published_year=2003, pages="309", language="English (USA)", book_type="Hardcover", location="7", section="7", genre="Fiction", employee_id=2, date="2022-06-02", Price=1000, translator="Joanne Rowling", Title_untranslated="Harry Potter and the Philosophers Stone", translated_from="English", edition=1, number_of_copies=1)
 
-ISBN_list = mycursor.execute("select ISBN from Books")
-ISBN_potter = 9780590353403
-for x in ISBN_list:
-    if ISBN_potter not in x:
-        mycursor.execute("commit;")
+#call search records procedure
+print(admin.search_records(mycursor=mycursor, ISBN=9780593334833, Employee_id=None, Book_id=None))
 
-
-
-class Employee:
-    pass
+# ISBN_potter = 9780590353403
+# for x in ISBN_list:
+#     if ISBN_potter not in x:
+#         mycursor.execute("commit;")
 
 # def query_to_values(db, query):  # returns a list of values
 #     mycursor = db.cursor()
