288e93e967996daccec6cd54efa40434ad9752fd
gabrielius_d <94465193+gabrielius-d@users.noreply.github.com>
2022-06-16 12:46:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,53 +1,49 @@
 import tkinter as tk
-#here we will put names of files
+# here we will put names of files
 from Start_window import Start_window
 from login_window import login_window
 from Employees_sales_tab import Employee_sales_window
 from Admin_inventory_window import Admin_inventory_window
 from employee_window import employee_window
-from Admin_finance_window import Admin_finance_window
+
 
 class Handler(tk.Tk):
 
     def __init__(self, *args, **kwargs):
-
         tk.Tk.__init__(self, *args, **kwargs)
         container = tk.Frame(self)
         width = tk.Tk.winfo_screenwidth(self)
         height = tk.Tk.winfo_screenheight(self)
-        tk.Tk.geometry(self,f"{width}x{height}")
-        tk.Tk.title(self,"Amorlibrorum")
+        tk.Tk.geometry(self, f"{width}x{height}")
+        tk.Tk.title(self, "Amor Librorum – your favourite book shop")
+        tk.Tk.minsize(self, 800, 400)
 
-        container.pack(side="top", fill="both", expand= True)
+        container.pack(side="top", fill="both", expand=True)
 
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
 
         self.frames = {}
 
-#here we will put names of classes in respective files
+        # here we will put names of classes in respective files
 
-        for F in (Start_window, login_window, Employee_sales_window, Admin_inventory_window, employee_window, Admin_finance_window):
+        for F in (Start_window, login_window, Employee_sales_window, Admin_inventory_window, employee_window):
             print("here", F)
             frame = F(container, self)
 
             self.frames[F] = frame
 
-            frame.grid(row=0,column=0, sticky="nsew")
+            frame.grid(row=0, column=0, sticky="nsew")
 
         self.show_frame(Start_window)
 
     def show_frame(self, cont):
-
         frame = self.frames[cont]
         frame.tkraise()
 
 
-
-
-if __name__=='__main__':
+if __name__ == '__main__':
     LARGE_FONT = ("Verdana", 12)
-
 
     app = Handler()
     app.mainloop()
