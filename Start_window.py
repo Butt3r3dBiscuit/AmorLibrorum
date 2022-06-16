8586da0956db786f66a1f26f0b2c7c977b3536df
gabrielius_d <94465193+gabrielius-d@users.noreply.github.com>
2022-06-16 13:29:17 +0200

    rel_height to row_height

--- Start_window.py
+++ Start_window.py
@@ -1,12 +1,15 @@
 import tkinter as tk
 import login_window
 
+
 class Start_window(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
+        # tk.self.title("Title?")
+        # tk.Frame(self, "this is Start_window.py")
 
         rel_width = 0.1
-        rel_height = 0.2
+        row_height = 20
 
         # tk.title("Book search")
 
@@ -14,17 +17,14 @@ class Start_window(tk.Frame):
         # window.geometry("800x400")
         # window.title("start Window")
 
+        Login = tk.Button(self, text="Log in", height=2, width=11, command=lambda: controller.show_frame(
+            login_window.login_window))
+        Login.place(relx=1, anchor='ne')
 
+        button_font = "Helvetica 18 bold"
 
-
-
-
-
-        Login = tk.Button(self, text="Log in", height=25, width=45, command=lambda: controller.show_frame(login_window.login_window))
-        Login.place(relx=1, relwidth=rel_width, relheight=rel_height, anchor='ne')
-
-        Book_search = tk.Label(self, text="Book search: ", font='Helvetica 18 bold')
-        Found = tk.Label(self, text="Found: ", font='Helvetica 18 bold')
+        Book_search = tk.Label(self, text="Book search: ", font=button_font)
+        Found = tk.Label(self, text="Found: ", font=button_font)
         Title = tk.Label(self, text="Title")
         Author = tk.Label(self, text="Author")
         Edition = tk.Label(self, text="Edition")
@@ -35,23 +35,21 @@ class Start_window(tk.Frame):
         Sell_price = tk.Label(self, text="SellPrice")
         In_store = tk.Label(self, text="InStore")
 
-
-        Book_search.place(relx=0.2, rely=0.2, relwidth=0.2, relheight=rel_height, anchor="e")
-        Found.place(relx=0.2, rely=0.6, relwidth=0.2, relheight=rel_height, anchor="e")
-        Title.place(relx=0.1, rely=0.8, relwidth=rel_width, relheight=rel_height, anchor="e")
-        Author.place(relx=0.2, rely=0.8, relwidth=rel_width, relheight=rel_height, anchor="e")
-        Edition.place(relx=0.3, rely=0.8, relwidth=rel_width, relheight=rel_height, anchor="e")
-        Version.place(relx=0.4, rely=0.8, relwidth=rel_width, relheight=rel_height, anchor="e")
-        Location.place(relx=0.5, rely=0.8, relwidth=rel_width, relheight=rel_height, anchor="e")
-        Section.place(relx=0.6, rely=0.8, relwidth=rel_width, relheight=rel_height, anchor="e")
-        Language.place(relx=0.7, rely=0.8, relwidth=rel_width, relheight=rel_height, anchor="e")
-        Sell_price.place(relx=0.8, rely=0.8, relwidth=rel_width, relheight=rel_height, anchor="e")
-        In_store.place(relx=0.9, rely=0.8, relwidth=rel_width, relheight=rel_height, anchor="e")
+        Book_search.place(relx=0.2, rely=0.2, relwidth=0.2, height=row_height, anchor="e")
+        Found.place(relx=0.2, rely=0.6, relwidth=0.2, height=row_height, anchor="e")
+        Title.place(relx=0.1, rely=0.8, relwidth=rel_width, height=row_height, anchor="e")
+        Author.place(relx=0.2, rely=0.8, relwidth=rel_width, height=row_height, anchor="e")
+        Edition.place(relx=0.3, rely=0.8, relwidth=rel_width, height=row_height, anchor="e")
+        Version.place(relx=0.4, rely=0.8, relwidth=rel_width, height=row_height, anchor="e")
+        Location.place(relx=0.5, rely=0.8, relwidth=rel_width, height=row_height, anchor="e")
+        Section.place(relx=0.6, rely=0.8, relwidth=rel_width, height=row_height, anchor="e")
+        Language.place(relx=0.7, rely=0.8, relwidth=rel_width, height=row_height, anchor="e")
+        Sell_price.place(relx=0.8, rely=0.8, relwidth=rel_width, height=row_height, anchor="e")
+        In_store.place(relx=0.9, rely=0.8, relwidth=rel_width, height=row_height, anchor="e")
 
         Book_text = tk.Text(self, height=1, width=300, borderwidth=1, relief="groove")
         Book_text.pack()
-        Book_text.place(relx=0.5, rely=0.4, relwidth=0.4, relheight=0.1, anchor="e")
+        Book_text.place(relx=0.5, rely=0.4, relwidth=0.4, height=row_height, anchor="e")
 
         Search = tk.Button(self, text="Search")
-        Search.place(relx=0.8, rely=0.4, relwidth=rel_width, relheight=0.1, anchor="e")
-
+        Search.place(relx=0.8, rely=0.4, relwidth=rel_width, height=row_height, anchor="e")
