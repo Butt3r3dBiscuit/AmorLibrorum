0c6c740c251b3b74e7b81c7b67255a84a2e32d3d
gabrielius_d <94465193+gabrielius-d@users.noreply.github.com>
2022-06-16 13:29:26 +0200

    Update text_box.py

--- text_box.py
+++ text_box.py
@@ -9,12 +9,14 @@ textbox.pack()
 # This is for demonstration purposes
 tk.Text(height=10, width=10).pack()
 
+
 def default(event):
     current = textbox.get("1.0", tk.END)
     if current == "Default\n":
         textbox.delete("1.0", tk.END)
     elif current == "\n":
         textbox.insert("1.0", "Default")
+
 
 textbox.bind("<FocusIn>", default)
 textbox.bind("<FocusOut>", default)
