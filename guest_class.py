9e07dfbc83614a39e885cfed7a29aaa44d9a3539
PauloRC123 <103492828+PauloRC123@users.noreply.github.com>
2022-06-16 13:15:52 +0200

    takes only 1 input now (and is way cleaner)

--- guest_class.py
+++ guest_class.py
@@ -9,57 +9,40 @@ cursor = db.cursor()
 class Guest:
     def __init__(self):
         pass
-        
-    def search(self, author_name="", author_surname="", title=""):
+
+    def search(self, search=""):
         conditions = ""
         cor_sub = "T.PRICE_IN_CENTS=(SELECT MIN(TR.PRICE_IN_CENTS) " \
                            "FROM TRANSACTIONS TR, BOOK_ENTRIES BEN, BOOKS BO " \
                            "WHERE TR.BOOK_ID=BEN.BOOK_ID " \
                            "AND BEN.ISBN=BO.ISBN " \
                            "AND BO.ISBN=B.ISBN "
-        if title == "" and author_name == "" and author_surname == "":
+        if search == "":
             conditions = f"WHERE {cor_sub}) "
-        if title != "":
+        else:
             cor_sub = f"{cor_sub}" \
-                      "AND (BO.ISBN IN (SELECT ISBN FROM BOOKS " \
-                                      f"WHERE TITLE LIKE '%{title}' " \
-                                      f"OR TITLE LIKE '{title}%') " \
+                      "AND ((BO.ISBN IN (SELECT ISBN FROM BOOKS " \
+                      f"WHERE TITLE LIKE '%{search}' " \
+                      f"OR TITLE LIKE '{search}%') " \
                       "OR BO.ISBN IN (SELECT ISBN FROM IF_TRANSLATED " \
-                                    f"WHERE TITLE_UNTRANSLATED LIKE '%{title}' " \
-                                    f"OR TITLE_UNTRANSLATED LIKE '{title}%')) "
-            conditions = f"WHERE B.TITLE LIKE '%{title}' " \
-                         f"OR B.TITLE LIKE '{title}%' " \
-                         f"OR IT.TITLE_UNTRANSLATED LIKE '%{title}' " \
-                         f"OR IT.TITLE_UNTRANSLATED LIKE '{title}%' " \
+                      f"WHERE TITLE_UNTRANSLATED LIKE '%{search}' " \
+                      f"OR TITLE_UNTRANSLATED LIKE '{search}%'))) " \
+                      "OR (BO.ISBN IN (SELECT ISBN FROM AUTHORS " \
+                      f"WHERE AUTHOR_NAME LIKE '%{search}' " \
+                      f"OR AUTHOR_NAME LIKE '{search}%')) " \
+                      "OR (BO.ISBN IN (SELECT ISBN FROM AUTHORS " \
+                      f"WHERE AUTHOR_SURNAME LIKE '%{search}' " \
+                      f"OR AUTHOR_SURNAME LIKE '{search}%')) "
+            conditions = f"WHERE (B.TITLE LIKE '%{search}' " \
+                         f"OR B.TITLE LIKE '{search}%' " \
+                         f"OR IT.TITLE_UNTRANSLATED LIKE '%{search}' " \
+                         f"OR IT.TITLE_UNTRANSLATED LIKE '{search}%') " \
+                         f"OR (A.AUTHOR_NAME LIKE '%{search}' " \
+                         f"OR A.AUTHOR_NAME LIKE '{search}%') " \
+                         f"OR (A.AUTHOR_SURNAME LIKE '%{search}' " \
+                         f"OR A.AUTHOR_SURNAME LIKE '{search}%') " \
                          f"AND {cor_sub}) "
-        if author_name != "" and author_surname == "":
-            cor_sub = f"{cor_sub}" \
-                      "AND BO.ISBN IN (SELECT ISBN FROM AUTHORS " \
-                                     f"WHERE AUTHOR_NAME LIKE '%{author_name}' " \
-                                     f"OR AUTHOR_NAME LIKE '{author_name}%') "
-            conditions = f"WHERE A.AUTHOR_NAME LIKE '%{author_name}' " \
-                         f"OR A.AUTHOR_NAME LIKE '{author_name}%' " \
-                         f"AND {cor_sub}) "
-        if author_surname != "" and author_name == "":
-            cor_sub = f"{cor_sub}" \
-                      "AND BO.ISBN IN (SELECT ISBN FROM AUTHORS " \
-                                     f"WHERE AUTHOR_SURNAME LIKE '%{author_surname}' " \
-                                     f"OR AUTHOR_SURNAME LIKE '{author_surname}%') "
-            conditions = f"WHERE A.AUTHOR_SURNAME LIKE '%{author_surname}' " \
-                         f"OR A.AUTHOR_SURNAME LIKE '{author_surname}%' " \
-                         f"AND {cor_sub}) "
-        if author_name != "" and author_surname != "":
-            cor_sub = f"{cor_sub}" \
-                      "AND BO.ISBN IN (SELECT ISBN FROM AUTHORS " \
-                                     f"WHERE (AUTHOR_NAME LIKE '%{author_name}' " \
-                                     f"OR AUTHOR_NAME LIKE '{author_name}%') " \
-                                     f"AND (AUTHOR_SURNAME LIKE '%{author_surname}' " \
-                                     f"OR AUTHOR_SURNAME LIKE '{author_surname}%')) "
-            conditions = f"WHERE (A.AUTHOR_NAME LIKE '%{author_name}' " \
-                                f"OR A.AUTHOR_NAME LIKE '{author_name}%') " \
-                         f"AND (A.AUTHOR_SURNAME LIKE '%{author_surname}' " \
-                              f"OR A.AUTHOR_SURNAME LIKE '{author_surname}%') " \
-                         f"AND {cor_sub}) "
+
         cursor.execute("SELECT B.TITLE, A.AUTHOR_NAME, A.AUTHOR_SURNAME, B.LANGUAGE, B.GENRE, B.LOCATION, B.EDITION, B.BOOK_TYPE, T.PRICE_IN_CENTS, COUNT(BE.BOOK_ID) " \
                        "FROM BOOKS B LEFT JOIN AUTHORS A " 
                        "ON B.ISBN=A.ISBN " 
@@ -73,6 +56,6 @@ class Guest:
                        "GROUP BY B.ISBN")
         result = cursor.fetchall()
         return result
-    
+
 Test = Guest()
-print(Test.search("", "", "It Ends"))
+print(Test.search(""))
