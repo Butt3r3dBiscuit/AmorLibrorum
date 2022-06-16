1f3e0103495f72af649b13a28d228cb718290918
gabrielius_d <94465193+gabrielius-d@users.noreply.github.com>
2022-06-16 16:40:20 +0200

    Update Employees_sales_tab.py

--- Employees_sales_tab.py
+++ Employees_sales_tab.py
@@ -3,7 +3,6 @@ import Start_window
 
 emp_id = None
 
-
 class Employee_sales_window(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
@@ -20,13 +19,13 @@ class Employee_sales_window(tk.Frame):
         Sell = tk.Button(self, text="Sell")
         Sell.place(relx=1, rely=0.9, relwidth=rel_width, height=2, anchor="e")
 
-        Log_out = tk.Button(self, text="Log out",
-                            command=lambda: controller.show_frame(Start_window.Start_window))
-        Log_out.place(relx=1, rely=0, relwidth=rel_width, height=2, anchor="ne")
+        Log_out = tk.Button(self, text = "Log out",
+                            command = lambda: controller.show_frame(Start_window.Start_window))
+        Log_out.place(relx = 1, rely = 0, relwidth = rel_width, height = 2, anchor = "ne")
 
-        Book_label = tk.Label(self, text="Book ID", width = "15")
+        Book_label = tk.Label(self, text = "Book ID", width = "15")
         Book_label.pack()
-        Book_text = tk.Text(self, borderwidth=1, relief="groove")
+        Book_text = tk.Text(self, borderwidth = 1, relief = "groove")
         Book_text.pack()
         Book_text.place(relx=0.3, rely=0.35, relwidth=0.2, relheight=0.1, anchor="e")
         Book_label.place(relx=0.2, rely=0.25, relwidth=rel_width, relheight=rel_height, anchor="e")
