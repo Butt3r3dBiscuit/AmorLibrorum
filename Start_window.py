e8044ba32273b66bdeb629df35fb6b614bcbc708
gabrielius_d <94465193+gabrielius-d@users.noreply.github.com>
2022-06-16 16:56:35 +0200

    Update Start_window.py

--- Start_window.py
+++ Start_window.py
@@ -34,6 +34,7 @@ class Start_window(tk.Frame):
         Sell_price = tk.Label(self, text="SellPrice")
         In_store = tk.Label(self, text="InStore")
 
+
         Book_search.place(relx=0.2, rely=0.2, relwidth=0.2, height=row_height, anchor="e")
         Found.place(relx=0.2, rely=0.6, relwidth=0.2, height=row_height, anchor="e")
         Title.place(relx=0.1, rely=0.8, relwidth=rel_width, height=row_height, anchor="e")
@@ -46,9 +47,35 @@ class Start_window(tk.Frame):
         Sell_price.place(relx=0.8, rely=0.8, relwidth=rel_width, height=row_height, anchor="e")
         In_store.place(relx=0.9, rely=0.8, relwidth=rel_width, height=row_height, anchor="e")
 
-        Book_text = tk.Text(self, height=1, width=300, borderwidth=1, relief="groove")
-        Book_text.pack()
-        Book_text.place(relx=0.5, rely=0.4, relwidth=0.4, height=row_height, anchor="e")
+        self.Book_text = tk.Entry(self, width=300, borderwidth=1, relief="groove")
+        self.Book_text.pack()
+        self.Book_text.place(relx=0.5, rely=0.4, relwidth=0.4, height=0.1, anchor="e")
+
+        books = []
+        books_var = tk.StringVar(value=books)
+        self.Books_found = tk.Listbox(self, listvariable=books_var, selectmode="extended")
+        self.Books_found.place(relx=0.03, rely=0.45, relwidth=0.85, height=0.5)
+
+        Search = tk.Button(self, text="Search", command=self.search)
+        Search.place(relx=0.8, rely=0.2, relwidth=rel_width, height=0.1, anchor="e")
+
+    def search(self):
+        a = self.Book_text.get()
+        print(f"({a})")
+        Booksearch = gc.Guest()
+        b = Booksearch.search(a)
+        print(b)
+        m = len(b)
+        books = []
+        for i in range(m):
+            books.append(str(b[i]))
+        print(books)
+        books_var = tk.StringVar(value=books)
+        self.Books_found.config(listvariable=books_var)
+
+
+
+# h.place(relx=0.8, rely=0.4, relwidth=rel_width, height=0.1, anchor="e")
 
         Search = tk.Button(self, text="Search")
         Search.place(relx=0.8, rely=0.4, relwidth=rel_width, height=row_height, anchor="e")
