be0b827d9c8bbf25d54508cb91d9fe9170832bb2
vortex552 <nielsvwinden@gmail.com>
2022-06-17 14:29:26 +0200

    some triggers and procedures

---
+++ sql procedures and triggers/sell_trigger.sql
@@ -0,0 +1,25 @@
+DELIMITER $$
+
+CREATE TRIGGER before_sold
+BEFORE INSERT
+ON transactions FOR EACH ROW
+BEGIN
+    DECLARE boughtcount INT;
+    DEClARE soldcount INT;
+    DECLARE message_text varchar(255);
+
+    SELECT COUNT(*) 
+    INTO boughtcount
+    FROM transactions WHERE Price_in_cents<0 and book_id=NEW.book_id;
+    
+    SELECT COUNT(*) 
+    INTO soldcount
+    FROM transactions WHERE Price_in_cents>=0 and book_id=NEW.book_id;
+
+    IF boughtcount < soldcount THEN
+        signal sqlstate '45000' SET message_text="Book cannot be sold more times than it is bought >:(";
+    END IF; 
+
+END $$
+
+DELIMITER ;
