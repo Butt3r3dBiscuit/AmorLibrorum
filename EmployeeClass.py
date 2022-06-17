b895ec00ea624917cf396a290e16cd946037aefe
vortex552 <nielsvwinden@gmail.com>
2022-06-18 00:52:31 +0200

    sell trigger and employee class sell/return fix

--- EmployeeClass.py
+++ EmployeeClass.py
@@ -6,16 +6,21 @@ from user_creation import employee_user_addition
 class Employee:
     #database cursor
     def __init__(self, db):
+        self.db = db
         self.mycursor = db.cursor()
     #sell procedure
     def sell(self, book_id, employee_id):
-        self.mycursor.execute(f"CALL sell({book_id}, {employee_id})")
+        arg=(book_id, employee_id)
+        self.mycursor.callproc("sell", arg)
     def return_book(self, book_id, employee_id):
-        self.mycursor.execute(f"CALL return_book({book_id}, {employee_id})")
+        arg=(book_id, employee_id)
+        self.mycursor.callproc("return_book", arg)
 
 if __name__ == "__main__":
     db = connect.connect_admin("MyN3wP4ssw0rd!*")
     emp = Employee(db)
     mycursor= db.cursor()
 
-    emp.sell(20, 1)
+    emp.sell(21, 2)
+    db.commit()
+    db.close()
