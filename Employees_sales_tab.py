4f427129f6b649dd67d8d19f249b5e4d747b3964
gabrielius_d <94465193+gabrielius-d@users.noreply.github.com>
2022-06-17 18:02:49 +0200

    Update Employees_sales_tab.py

--- Employees_sales_tab.py
+++ Employees_sales_tab.py
@@ -9,8 +9,10 @@ class Employee_sales_window(tk.Frame):
 
         rel_width = 0.1
         row_height = 20
+        button_height = 45
+        text_height = 20
+        title_height = 30
 
-        button_font = "Helvetica 18 bold"
 
         Search = tk.Button(self, text = "Search")
         Search.place(relx=0.4, rely=0.35, relwidth=rel_width, height=20, anchor="e")
@@ -24,39 +26,5 @@ class Employee_sales_window(tk.Frame):
 
         Book_label = tk.Label(self, text = "Book ID", width = "15")
         Book_label.pack()
-        Book_text = tk.Text(self, borderwidth = 1, relief = "groove")
+        Book_text = tk.Text(self, borderwidth=1, relief="groove")
         Book_text.pack()
-        Book_text.place(relx=0.3, rely=0.35, relwidth=0.2, height=row_height, anchor="e")
-        Book_label.place(relx=0.2, rely=0.25, relwidth=rel_width, height=row_height, anchor="e")
-
-
-        Book_search = tk.Label(self, text = "Book search: ", font = button_font)
-        # Found = tk.Label(self, text = "Found: ", font = button_font)
-        # Title = tk.Label(self, text = "Title")
-        # Author = tk.Label(self, text = "Author")
-        # Edition = tk.Label(self, text = "Edition")
-        # Version = tk.Label(self, text = "Version")
-        # Location = tk.Label(self, text = "Location")
-        # Section = tk.Label(self, text = "Section")
-        # Language = tk.Label(self, text = "Language")
-        # Sell_price = tk.Label(self, text = "SellPrice")
-        # In_store = tk.Label(self, text = "InStore")
-
-
-        Book_search.place(relx=0.25, rely=0.1, relwidth=0.2, height=row_height, anchor="e")
-        # Found.place(relx=0.2, rely=0.5, relwidth=0.2, height=row_height, anchor="e")
-        # Title.place(relx=0.1, rely=0.7, relwidth=rel_width, height=row_height, anchor="e")
-        # Author.place(relx=0.2, rely=0.7, relwidth=rel_width, height=row_height, anchor="e")
-        # Edition.place(relx=0.3, rely=0.7, relwidth=rel_width, height=row_height, anchor="e")
-        # Version.place(relx=0.4, rely=0.7, relwidth=rel_width, height=row_height, anchor="e")
-        # Location.place(relx=0.5, rely=0.7, relwidth=rel_width, height=row_height, anchor="e")
-        # Section.place(relx=0.6, rely=0.7, relwidth=rel_width, height=row_height, anchor="e")
-        # Language.place(relx=0.7, rely=0.7, relwidth=rel_width, height=row_height, anchor="e")
-        # Sell_price.place(relx=0.8, rely=0.7, relwidth=rel_width, height=row_height, anchor="e")
-        # In_store.place(relx=0.9, rely=0.7, relwidth=rel_width, height=row_height, anchor="e")
-
-
-
-
-        # window.mainloop()
-
