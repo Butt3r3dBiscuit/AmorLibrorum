f5bcdc2819e32d4f98bd472b437a34a7af577d5a
gabrielius_d <94465193+gabrielius-d@users.noreply.github.com>
2022-06-17 17:51:39 +0200

    Update Start_window.py

--- Start_window.py
+++ Start_window.py
@@ -10,28 +10,30 @@ class Start_window(tk.Frame):
         rel_width = 0.1
 <<<<<<< Updated upstream
         row_height = 20
+        button_font = "Helvetica 18 bold"
+        text_height = 20
+        title_height = 40
 
 
         Login = tk.Button(self, text="Log in", height=2, width=11, command=lambda: controller.show_frame(
             login_window.login_window))
         Login.place(relx=1, anchor='ne')
 
-        button_font = "Helvetica 18 bold"
-
         Book_search = tk.Label(self, text="Book search: ", font=button_font)
-        Found = tk.Label(self, text="Found: ", font=button_font)
-        Title = tk.Label(self, text="Title")
-        Author = tk.Label(self, text="Author")
-        Edition = tk.Label(self, text="Edition")
-        Version = tk.Label(self, text="Version")
-        Location = tk.Label(self, text="Location")
-        Section = tk.Label(self, text="Section")
-        Language = tk.Label(self, text="Language")
-        Sell_price = tk.Label(self, text="SellPrice")
-        In_store = tk.Label(self, text="InStore")
+        # Found = tk.Label(self, text="Found: ", font=button_font)
+        # Title = tk.Label(self, text="Title")
+        # Author = tk.Label(self, text="Author")
+        # Edition = tk.Label(self, text="Edition")
+        # Version = tk.Label(self, text="Version")
+        # Location = tk.Label(self, text="Location")
+        # Section = tk.Label(self, text="Section")
+        # Language = tk.Label(self, text="Language")
+        # Sell_price = tk.Label(self, text="SellPrice")
+        # In_store = tk.Label(self, text="InStore")
 
 
         Book_search.place(relx=0.2, rely=0.2, relwidth=0.2, height=row_height, anchor="e")
+<<<<<<< Updated upstream
         Found.place(relx=0.2, rely=0.6, relwidth=0.2, height=row_height, anchor="e")
         Title.place(relx=0.1, rely=0.8, relwidth=rel_width, height=row_height, anchor="e")
         Author.place(relx=0.2, rely=0.8, relwidth=rel_width, height=row_height, anchor="e")
@@ -148,6 +150,18 @@ class Start_window(tk.Frame):
         # Sell_price.place(relx=0.8, rely=0.8, relwidth=rel_width, relheight=rel_height, anchor="e")
         # In_store.place(relx=0.9, rely=0.8, relwidth=rel_width, relheight=rel_height, anchor="e")
 >>>>>>> Stashed changes
+=======
+        # Found.place(relx=0.2, rely=0.6, relwidth=0.2, height=row_height, anchor="e")
+        # Title.place(relx=0.1, rely=0.8, relwidth=rel_width, height=row_height, anchor="e")
+        # Author.place(relx=0.2, rely=0.8, relwidth=rel_width, height=row_height, anchor="e")
+        # Edition.place(relx=0.3, rely=0.8, relwidth=rel_width, height=row_height, anchor="e")
+        # Version.place(relx=0.4, rely=0.8, relwidth=rel_width, height=row_height, anchor="e")
+        # Location.place(relx=0.5, rely=0.8, relwidth=rel_width, height=row_height, anchor="e")
+        # Section.place(relx=0.6, rely=0.8, relwidth=rel_width, height=row_height, anchor="e")
+        # Language.place(relx=0.7, rely=0.8, relwidth=rel_width, height=row_height, anchor="e")
+        # Sell_price.place(relx=0.8, rely=0.8, relwidth=rel_width, height=row_height, anchor="e")
+        # In_store.place(relx=0.9, rely=0.8, relwidth=rel_width, height=row_height, anchor="e")
+>>>>>>> Stashed changes
 
         self.Book_text = tk.Entry(self, width=300, borderwidth=1, relief="groove")
         self.Book_text.pack()
@@ -156,7 +170,7 @@ class Start_window(tk.Frame):
         books = []
         books_var = tk.StringVar(value=books)
         self.Books_found = tk.Listbox(self, listvariable=books_var, selectmode="extended")
-        self.Books_found.place(relx=0.03, rely=0.45, relwidth=0.85, height=500)
+        self.Books_found.place(relx=0.03, rely=0.45, relwidth=0.85, height=100)
 
         Search = tk.Button(self, text="Search", command=self.search)
         Search.place(relx=0.8, rely=0.2, relwidth=rel_width, height=50, anchor="e")
