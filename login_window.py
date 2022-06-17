9d91381608e508e60ce6ac59bc42151cb175bde2
gabrielius_d <94465193+gabrielius-d@users.noreply.github.com>
2022-06-17 14:46:26 +0200

    just whitespace -.-

--- login_window.py
+++ login_window.py
@@ -4,9 +4,10 @@ import Employees_sales_tab
 from connect import connect_employee
 import Admin_inventory_window
 
+
 class login_window(tk.Frame):
     def __init__(self, parent, controller):
-        tk.Frame.__init__(self,parent)
+        tk.Frame.__init__(self, parent)
 
         # root = tk.Tk
         print(Admin_inventory_window.emp_id)
@@ -18,10 +19,10 @@ class login_window(tk.Frame):
         # root.geometry('300x150')
         # root.title('Log in')
         # root.resizable(False, False)
-        back_button = tk.Button(self, text="Return home",height=2, width=11,
+        back_button = tk.Button(self, text="Return home", height=2, width=11,
                                 command= lambda: self.back_button(controller))
         back_button.pack()
-        back_button.place(x=0,y=0)
+        back_button.place(x=0, y=0)
 
 
         email_label = tk.Label(self, text="Email Adress", width = "15")
@@ -42,7 +43,7 @@ class login_window(tk.Frame):
     def log_in(self, controller):
         email = self.email_text.get()
         print("email: ", email)
-        if email=="margje@amorlibrorum.boek":
+        if email == "margje@amorlibrorum.boek":
             print(True)
         else:
             print([email])
@@ -52,14 +53,14 @@ class login_window(tk.Frame):
         # password = "YetAn0!herqwertyp4ssword" #temp
         email = "frank@amorlibrorum.boek"
         password = "An0!herqwertyp4ssword"
-        self.db = connect_employee(email,password)
+        self.db = connect_employee(email, password)
         if self.db == 1045:
             try:
                 self.error_label.destroy()
-                self.error_label = tk.Label(self, text="User not found!", width="15", fg="red")
+                self.error_label = tk.Label(self, text="User not found!", width = "15", fg = "red")
             except AttributeError:
                 print("label has not yet been created")
-            self.error_label = tk.Label(self, text="User not found!", width = "15", fg="red")
+            self.error_label = tk.Label(self, text="User not found!", width = "15", fg = "red")
             # error_label.pack()
             self.error_label.place(relx=0.5, rely=0.15, anchor="n")
         else:
@@ -73,11 +74,12 @@ class login_window(tk.Frame):
             self.new_window(position,controller)
             # success_label = tk.Label(self, text=f"User found! Position {position}", width="30", fg="green")
             # success_label.pack()
+
     def new_window(self,position, controller):
         self.password_text.destroy()
         self.password_text = tk.Entry(self, width=40, show="*")
         self.password_text.pack()
-        if position=="Staff":
+        if position == "Staff":
             try:
                 self.error_label.destroy()
             except AttributeError:
@@ -85,7 +87,7 @@ class login_window(tk.Frame):
             Employees_sales_tab.emp_id = self.employee_id
             print(Employees_sales_tab.emp_id)
             controller.show_frame(Employees_sales_tab.Employee_sales_window)
-        elif position=="Manager":
+        elif position == "Manager":
             try:
                 self.error_label.destroy()
             except AttributeError:
@@ -94,6 +96,7 @@ class login_window(tk.Frame):
             Admin_inventory_window.db = self.db
             print(Admin_inventory_window.emp_id)
             controller.show_frame(Admin_inventory_window.Admin_inventory_window)
+
     def back_button(self, controller):
         try:
             self.error_label.destroy()
