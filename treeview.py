39d53df55e995894b1b70b660639a4c053e28b7e
Honza0105 <honzajelinek@email.cz>
2022-06-17 13:12:52 +0200

    test treeview

---
+++ treeview.py
@@ -0,0 +1,22 @@
+from tkinter import *
+from tkinter import ttk
+
+root = Tk()
+root.geometry("500x500")
+
+my_tree = ttk.Treeview(root)
+
+my_tree['columns']= ("Name", "ID","Favorite Pizza")
+
+#columns
+my_tree.column("#0", width=120, minwidth=25)
+my_tree.column("Name", anchor=W, width=120)
+my_tree.column("ID", anchor=CENTER, width=80)
+my_tree.column("Favourite Pizza", anchor=W, width=120)
+
+#heading
+my_tree.heading("#0", text"Label", anchor=W)
+my_tree.heading("Name", text"Name", anchor=W)
+
+
+root.mainloop()
