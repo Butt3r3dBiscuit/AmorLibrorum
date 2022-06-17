7c5b2878a8701ab9b8d3665cd38e17b5d50e7ec0
gabrielius_d <94465193+gabrielius-d@users.noreply.github.com>
2022-06-17 18:04:12 +0200

    Update Employees_sales_tab.py

--- Employees_sales_tab.py
+++ Employees_sales_tab.py
@@ -13,18 +13,17 @@ class Employee_sales_window(tk.Frame):
         text_height = 20
         title_height = 30
 
+        Search = tk.Button(self, text="Search")
+        Search.place(relx=0.4, rely=0.35, relwidth=rel_width, relheight=0.1, anchor="e")
 
-        Search = tk.Button(self, text = "Search")
-        Search.place(relx=0.4, rely=0.35, relwidth=rel_width, height=20, anchor="e")
+        Sell = tk.Button(self, text="Sell")
+        Sell.place(relx=1, rely=0.9, relwidth=rel_width, relheight=0.1, anchor="e")
 
-        Sell = tk.Button(self, text = "Sell")
-        Sell.place(relx=1, rely=0.9, relwidth=rel_width, height=2, anchor="e")
+        Log_out = tk.Button(self, text="Log out",
+                            command=lambda: controller.show_frame(Start_window.Start_window))
+        Log_out.place(relx=1, rely=0, relwidth=rel_width,relheight=0.1, anchor="ne")
 
-        Log_out = tk.Button(self, text = "Log out",
-                            command = lambda: controller.show_frame(Start_window.Start_window))
-        Log_out.place(relx = 1, rely = 0, relwidth = rel_width, height=2, anchor="ne")
-
-        Book_label = tk.Label(self, text = "Book ID", width = "15")
+        Book_label = tk.Label(self, text="Book ID", width="15")
         Book_label.pack()
         Book_text = tk.Text(self, borderwidth=1, relief="groove")
         Book_text.pack()
