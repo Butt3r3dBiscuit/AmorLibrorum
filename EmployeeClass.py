d350b336ad4bec0c135036b5e6081ef3cb4185f9
vortex552 <nielsvwinden@gmail.com>
2022-06-18 13:26:47 +0200

    search records method

--- EmployeeClass.py
+++ EmployeeClass.py
@@ -2,6 +2,7 @@ from re import L
 import mysql.connector
 import connect
 from user_creation import employee_user_addition
+from AdminClass import func_search_records
 
 class Employee:
     #database cursor
@@ -12,9 +13,17 @@ class Employee:
     def sell(self, book_id, employee_id):
         arg=(book_id, employee_id)
         self.mycursor.callproc("sell", arg)
+    #return  book procedure
     def return_book(self, book_id, employee_id):
         arg=(book_id, employee_id)
         self.mycursor.callproc("return_book", arg)
+    #search records
+    def search_records(self, ISBN=None, Employee_id=None, Book_id=None):
+        #retrieves records
+        search = func_search_records(ISBN=ISBN, Employee_id=Employee_id, Book_id=Book_id)
+        self.mycursor.execute(search)
+        result_fetch = self.mycursor.fetchall()
+        return result_fetch
 
 if __name__ == "__main__":
     db = connect.connect_admin("MyN3wP4ssw0rd!*")
