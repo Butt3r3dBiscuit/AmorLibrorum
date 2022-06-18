a5956ab86a2f61ce826c7ae824ff6d405c9177e1
vortex552 <nielsvwinden@gmail.com>
2022-06-18 13:05:31 +0200

    oops no price deter

--- sql_procedures_and_triggers/return_book.sql
+++ sql_procedures_and_triggers/return_book.sql
@@ -4,7 +4,7 @@ CREATE PROCEDURE return_book(ID varchar(13), emp_id INT)
 BEGIN
 	DECLARE t_date date;
 	DECLARE Price int;
-	SET Price = -(select price_determination(ID));
+	SET Price = -1*(select price_in_cents from transactions where transaction_id=(select max(transaction_id) from transactions where book_id=ID));
 	SET t_date = (select current_date);
 	INSERT INTO transactions(Book_ID, Employee_ID, Trans_Date, Price_in_cents) values (ID, emp_id, t_date, Price);
 END &&
