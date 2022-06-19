18853c6500ea35f8b0fbe44d4bf044337107f4c4
vortex552 <nielsvwinden@gmail.com>
2022-06-20 00:36:06 +0200

    clean proc fix

--- functions_in_sql.sql
+++ functions_in_sql.sql
@@ -126,9 +126,11 @@ RETURN bool;
 END;
 
 -- deletes sold book records in transactions older than 5 years
-CREATE PROCEDURE clean()
+CREATE PROCEDURE cleann()
 BEGIN
 	DECLARE t_date date;
+	DECLARE d_date date;
 	SET t_date = (select current_date);
+	SET d_date = DATEADD(t_date, -5);
     DELETE FROM TRANSACTIONS WHERE Trans_Date<=t_date AND sold(book_id)=1;
 END;
--- sql_procedures_and_triggers/delete_5.sql
+++ sql_procedures_and_triggers/delete_5.sql
@@ -1,9 +1,11 @@
 DELIMITER &&
 
-CREATE PROCEDURE clean()
+CREATE PROCEDURE cleann()
 BEGIN
 	DECLARE t_date date;
+	DECLARE d_date date;
 	SET t_date = (select current_date);
+	SET d_date = DATEADD(t_date, -5);
     DELETE FROM TRANSACTIONS WHERE Trans_Date<=t_date AND sold(book_id)=1;
 END &&
 
