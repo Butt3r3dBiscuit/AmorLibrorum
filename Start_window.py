704f5e6a44a505cc7e2ef711241c9749f107d0cc
gabrielius_d <94465193+gabrielius-d@users.noreply.github.com>
2022-06-20 01:34:16 +0200

    Update Start_window.py

--- Start_window.py
+++ Start_window.py
@@ -9,12 +9,8 @@ class Start_window(tk.Frame):
         tk.Frame.__init__(self, parent)
 
         rel_width = 0.1
-        rel_height = 0.2
-        # rel_height = 0.05
         row_height = 20
         button_height = 45
-        text_height = 20
-        title_height = 30
         button_font = "Helvetica 18 bold"
 
         self.search_results = ttk.Treeview(self)
@@ -68,68 +64,21 @@ class Start_window(tk.Frame):
 
         self.search_results.place(relx=0.025, rely=0.45, relwidth=0.95, relheight=0.5)
 
-        # self.search_results.insert(parent='', index='end', iid=0, values=["Book Lovers","Emily Henry",
-        #                                                              "NULL", "English","Sisters Fiction, Romantic Comedy", "Berkley","paperback",
-        #                                                              "2022", "400",  "17-18",
-        #                                                              "1479", "2"])
-        # self.search_results.insert(parent='0', index='end', iid=1, values=("Book Lovers","Emily Henry",
-        #                                                              "NULL", "English","Sisters Fiction, Romantic Comedy", "Berkley","paperback",
-        #                                                              "2022", "400",  "17-18",
-        #                                                              "1479", "1"))
-        # self.search_results.insert(parent='0', index='end', iid=2, values=("Book Lovers","Emily Henry",
-        #                                                              "NULL", "English","Sisters Fiction, Romantic Comedy", "Berkley","paperback",
-        #                                                              "2022", "400",  "17-18",
-        #                                                              "1479", "1"))
-
-
 
         Login = tk.Button(self, text="Log in", height=25, width=45, command=lambda: controller.show_frame(login_window.login_window))
         Login.place(relx=1, relwidth=rel_width, height=button_height, anchor='ne')
 
-        # Found = tk.Label(self, text="Found: ", font=button_font)
         Book_search = tk.Label(self, text="Book search: ", font=button_font)
-        # Title = tk.Label(self, text="Title")
-        # Author = tk.Label(self, text="Author")
-        # Edition = tk.Label(self, text="Edition")
-        # Version = tk.Label(self, text="Version")
-        # Location = tk.Label(self, text="Location")
-        # Section = tk.Label(self, text="Section")
-        # Language = tk.Label(self, text="Language")
-        # Sell_price = tk.Label(self, text="SellPrice")
-        # In_store = tk.Label(self, text="InStore")
-
-
         Book_search.place(relx=0.2, rely=0.2, relwidth=0.2, height=row_height, anchor="e")
-        # Found.place(relx=0.2, rely=0.6, relwidth=0.2, relheight=rel_height, anchor="e")
-        # Title.place(relx=0.1, rely=0.8, relwidth=rel_width, relheight=rel_height, anchor="e")
-        # Author.place(relx=0.2, rely=0.8, relwidth=rel_width, relheight=rel_height, anchor="e")
-        # Edition.place(relx=0.3, rely=0.8, relwidth=rel_width, relheight=rel_height, anchor="e")
-        # Version.place(relx=0.4, rely=0.8, relwidth=rel_width, relheight=rel_height, anchor="e")
-        # Location.place(relx=0.5, rely=0.8, relwidth=rel_width, relheight=rel_height, anchor="e")
-        # Section.place(relx=0.6, rely=0.8, relwidth=rel_width, relheight=rel_height, anchor="e")
-        # Language.place(relx=0.7, rely=0.8, relwidth=rel_width, relheight=rel_height, anchor="e")
-        # Sell_price.place(relx=0.8, rely=0.8, relwidth=rel_width, relheight=rel_height, anchor="e")
-        # In_store.place(relx=0.9, rely=0.8, relwidth=rel_width, relheight=rel_height, anchor="e")
 
         self.Book_text = tk.Entry(self, width=300, borderwidth=1, relief="groove")
         self.Book_text.pack()
-        self.Book_text.place(relx=0.5, rely=0.4, relwidth=0.1, height=row_height, anchor="e")
-
-        # books = []
-        # books_var = tk.StringVar(value=books)
-        # self.Books_found = tk.Listbox(self, listvariable=books_var, selectmode="extended")
-        # self.Books_found.place(relx=0.03, rely=0.45, relwidth=0.85, relheight=0.5)
-
-
-
-
-
-
+        self.Book_text.place(relx=0.2, rely=0.3, relwidth=0.1, height=row_height, anchor="e")
 
 
         Search = tk.Button(self, text="Search", command=self.search)
-        Search.place(relx=0.8, rely=0.2, relwidth=rel_width, height=row_height, anchor="e")
-
+        Search.pack()
+        Search.place(relx=0.3, rely=0.3, relwidth=rel_width, height=row_height, anchor="e")
 
 
 
@@ -171,5 +120,3 @@ class Start_window(tk.Frame):
                         values.append('')
             self.search_results.insert(parent=parent, index='end', iid=str(count), values=values)
             count += 1
-
-# h.place(relx=0.8, rely=0.4, relwidth=rel_width, relheight=0.1, anchor="e")
