36d6794f05c1558dc8cc8b195cd3a4e06e49dfe1
vortex552 <nielsvwinden@gmail.com>
2022-06-19 23:59:35 +0200

    trigger update transactions

--- functions_in_sql.sql
+++ functions_in_sql.sql
@@ -76,7 +76,7 @@ BEGIN
     INTO soldcount
     FROM transactions WHERE Price_in_cents>=0 and book_id=NEW.book_id;
 
-    IF (boughtcount<=soldcount) AND (NEW.Price_in_cents>0) THEN
+    IF (boughtcount<soldcount) AND (NEW.Price_in_cents>0) THEN
         signal sqlstate '45000' SET message_text = 'Book cannot be sold more times than it is bought >:(';
     END IF; 
 END;
