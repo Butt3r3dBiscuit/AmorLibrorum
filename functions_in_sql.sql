f174edc1e73fcd5653e7c8939dff2eee67ea4d34
vortex552 <nielsvwinden@gmail.com>
2022-06-20 00:42:21 +0200

    clean fix hopefully

--- functions_in_sql.sql
+++ functions_in_sql.sql
@@ -131,6 +131,6 @@ BEGIN
 	DECLARE t_date date;
 	DECLARE d_date date;
 	SET t_date = (select current_date);
-	SET d_date = DATEADD(t_date, -5);
+	SET d_date = DATEADD(year, -5, t_date);
     DELETE FROM TRANSACTIONS WHERE Trans_Date<=t_date AND sold(book_id)=1;
 END;
