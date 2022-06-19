95e326e240b06dfa7c3fe38cb75ae7e6aca68701
Honza0105 <honzajelinek@email.cz>
2022-06-19 13:53:54 +0200

    Admin employee - update

--- Admin_employee_window.py
+++ Admin_employee_window.py
@@ -32,8 +32,11 @@ class Admin_employee_window(tk.Frame):
 
         button_font = "Helvetica 18 bold"
 
-        Set_sellprice = tk.Label(self, text="Search Employee: ", font=button_font)
-        Set_sellprice.place(relx=0.2, rely=0.45, relwidth=rel_width, height=row_height, anchor="e")
+        Add_employee_label = tk.Label(self, text="Add Employee:", font=button_font)
+        Search_employee_label = tk.Label(self, text="Search Employee:", font=button_font)
+
+        Add_employee_label.place(relx=0.09, rely=0.15, relwidth=0.15, height=row_height, anchor="w")
+        Search_employee_label.place(relx=0.1, rely=0.35, relwidth=0.15, height=row_height, anchor="w")
 
         
         #Text
@@ -53,13 +56,58 @@ class Admin_employee_window(tk.Frame):
         Log_out.place(relx=0, rely=0, relwidth=rel_width, height=row_height, anchor="nw")
 
 
+        #
+        First_name_label3 = tk.Label(self, text="First name", width="15")
+        First_name_label3.pack()
+        self.First_name__text3 = tk.Entry(self, width=30, borderwidth=1, relief="groove")
+        self.First_name__text3.pack()
 
+        Last_name_label3 = tk.Label(self, text="Last name", width="15")
+        Last_name_label3.pack()
+        self.Last_name_entry3 = tk.Entry(self, width=30, borderwidth=1, relief="groove")
+        self.Last_name_entry3.pack()
+
+        Email_label = tk.Label(self, text="Email", width="15")
+        Last_name_label3.pack()
+        self.Email_entry = tk.Entry(self, width=30, borderwidth=1, relief="groove")
+        self.Email_entry.pack()
+
+        Password_label = tk.Label(self, text="Password", width="15")
+        Password_label.pack()
+        self.Password_entry = tk.Entry(self, width=30, borderwidth=1, relief="groove")
+        self.Password_entry.pack()
+
+        self.First_name__text3.place(relx=0.215, rely=0.25, relwidth=rel_width, height=row_height, anchor="e")
+        First_name_label3.place(relx=0.215, rely=0.2, relwidth=rel_width, height=row_height, anchor="e")
+        Last_name_label3.place(relx=0.315, rely=0.2, relwidth=rel_width, height=row_height, anchor="e")
+        self.Last_name_entry3.place(relx=0.315, rely=0.25, relwidth=rel_width, height=row_height, anchor="e")
+        Email_label.place(relx=0.415, rely=0.2, relwidth=rel_width, height=row_height, anchor="e")
+        self.Email_entry.place(relx=0.415, rely=0.25, relwidth=rel_width, height=row_height, anchor="e")
+        Password_label.place(relx=0.515, rely=0.2, relwidth=rel_width, height=row_height, anchor="e")
+        self.Password_entry.place(relx=0.515, rely=0.25, relwidth=rel_width, height=row_height, anchor="e")
+
+
+
+        First_name_label_search = tk.Label(self, text="First name", width="15")
+        First_name_label_search.pack()
+        self.First_name_entry_search = tk.Entry(self, width=30, borderwidth=1, relief="groove")
+        self.First_name_entry_search.pack()
+
+        Last_name_label_search = tk.Label(self, text="Last name", width="15")
+        Last_name_label_search.pack()
+        self.Last_name_entry_search = tk.Entry(self, width=30, borderwidth=1, relief="groove")
+        self.Last_name_entry_search.pack()
+
+        self.First_name_entry_search.place(relx=0.215, rely=0.45, relwidth=rel_width, height=row_height, anchor="e")
+        First_name_label_search.place(relx=0.215, rely=0.4, relwidth=rel_width, height=row_height, anchor="e")
+        Last_name_label_search.place(relx=0.315, rely=0.4, relwidth=rel_width, height=row_height, anchor="e")
+        self.Last_name_entry_search.place(relx=0.315, rely=0.45, relwidth=rel_width, height=row_height, anchor="e")
 
         # #Placement Text
         # Book_search.place(relx=0.18, rely=0.15, relwidth=rel_width, relheight=rol_height, anchor="e")
         # Title.place(relx=0.2, rely=0.3, relwidth=rel_width, relheight=rol_height, anchor="e")
         # Author.place(relx=0.35, rely=0.3, relwidth=rel_width, relheight=rol_height, anchor="e")
-        # Editor.place(relx=0.5, rely=0.3, relwidth=rel_width, relheight=rol_height, anchor="e")
+        # Editor.place(relx=0.3, rely=0.3, relwidth=rel_width, relheight=rol_height, anchor="e")
         # Version.place(relx=0.65, rely=0.3, relwidth=rel_width, relheight=rol_height, anchor="e")
         # Language.place(relx=0.8, rely=0.3, relwidth=rel_width, relheight=rol_height, anchor="e")
         # All.place(relx=0.95, rely=0.3, relwidth=rel_width, relheight=rol_height, anchor="e")
