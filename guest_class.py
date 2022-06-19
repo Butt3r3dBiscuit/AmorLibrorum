5a2069a5306afd151c702d9344a859508ab69bd5
PauloRC123 <103492828+PauloRC123@users.noreply.github.com>
2022-06-19 22:22:55 +0200

    Update guest_class.py

--- guest_class.py
+++ guest_class.py
@@ -10,14 +10,17 @@ class Guest:
     def __init__(self):
         pass
 
-    def search(self, search=""):
+        def search(self, search, type):
         conditions = ""
+        or_isbn = ""
+        if type != 0:
+            or_isbn = f"OR B.ISBN LIKE '%{search}%'"
         if search != "":
             conditions = f"WHERE B.TITLE LIKE '%{search}%' " \
                          f"OR IT.TITLE_UNTRANSLATED LIKE '%{search}%' " \
                          f"OR A.AUTHOR_NAME LIKE '%{search}%' " \
-                         f"OR A.AUTHOR_SURNAME LIKE '%{search}%' "
-
+                         f"OR A.AUTHOR_SURNAME LIKE '%{search}%' " \
+                         f"{or_isbn}"
         cursor.execute("SET sql_mode = ''")
 
         cursor.execute("SELECT B.ISBN, B.TITLE, IT.TITLE_UNTRANSLATED, A.AUTHOR_NAME, A.AUTHOR_SURNAME, IT.TRANSLATOR, B.EDITION, B.LANGUAGE, IT.TRANSLATED_FROM, B.GENRE, B.PUBLISHER, B.BOOK_TYPE, B.YEAR_PUBLISHED, B.PAGES, B.LOCATION, B.SECTION, PRICE_DETERMINATION(BE.BOOK_ID), COUNT(BE.BOOK_ID) "
