a42479fa366d25ff0c70090f095b7d9099030118
gabrielius_d <94465193+gabrielius-d@users.noreply.github.com>
2022-06-19 16:35:00 +0200

    Update testdata.sql

--- testdata.sql
+++ testdata.sql
@@ -2,15 +2,15 @@ start transaction;
 
 LOCK TABLES `books` WRITE;
 /*!40000 ALTER TABLE `books` DISABLE KEYS */;
-INSERT INTO `books` VALUES (9781501110368, "It Ends with Us", "Atria", "2016", "384", "English", null, "paperback", "78", "58", "College Romance, Fiction");
-INSERT INTO `books` VALUES (9780679805274, "Oh, the Places You'll Go!","Random House Books for Young Readers","1990", "56", "English", null, null, "77", "78", "Children Classics");
-INSERT INTO `books` VALUES (9781476797090, "Phil: The Rip-Roaring \(and Unauthorized!\) Biography of Golf's Most Colorful Superstar","Avid Reader Press / Simon & Schuster","2022", "256", "English", null, "hardcover", "747", "178", "Biographies");
-INSERT INTO `books` VALUES (9780735211292, "Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones","Avery","2018", "320", "English", null, "hardcover", "11", "7", "Psychology");
-INSERT INTO `books` VALUES (9781538724731, "Verity","Grand Central Publishing","2021", "336", "English", null, "paperback", "74", "578", "Suspense, Thriller");
-INSERT INTO `books` VALUES (9781501161938, "The Seven Husbands of Evelyn Hugo: A Novel","Washington Square Press","2018", "400", "English", null, "paperback", "2", "718", "Holiday Romance");
-INSERT INTO `books` VALUES (9780593334833, "Book Lovers","Berkley","2022", "400", "English", null, "paperback", "17", "18", "Sisters Fiction, Romantic Comedy");
-INSERT INTO `books` VALUES (9780735219109, "Where the Crawdads Sing","Penguin Publishing Group","2021", "400", "English", null, "paperback", "7", "7", "Fiction");
-INSERT INTO `books` VALUES (9786094872082, "Priesaikos laužytojas", "BALTO leidybos namai", "2021", "320", "Lithuanian", 1, "hardcover", "2", "2", "Thriller, Suspense, Romantic");
+INSERT INTO `books` VALUES (9781501110368, "It Ends with Us", "Atria", "2016", "384", "English", null, "paperback", "A", "58", "College Romance, Fiction");
+INSERT INTO `books` VALUES (9780679805274, "Oh, the Places You'll Go!","Random House Books for Young Readers","1990", "56", "English", null, null, "K", "78", "Children Classics");
+INSERT INTO `books` VALUES (9781476797090, "Phil: The Rip-Roaring \(and Unauthorized!\) Biography of Golf's Most Colorful Superstar","Avid Reader Press / Simon & Schuster","2022", "256", "English", null, "hardcover", "E", "178", "Biographies");
+INSERT INTO `books` VALUES (9780735211292, "Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones","Avery","2018", "320", "English", null, "hardcover", "H", "7", "Psychology");
+INSERT INTO `books` VALUES (9781538724731, "Verity","Grand Central Publishing","2021", "336", "English", null, "paperback", "T", "578", "Suspense, Thriller");
+INSERT INTO `books` VALUES (9781501161938, "The Seven Husbands of Evelyn Hugo: A Novel","Washington Square Press","2018", "400", "English", null, "paperback", "P", "718", "Holiday Romance");
+INSERT INTO `books` VALUES (9780593334833, "Book Lovers","Berkley","2022", "400", "English", null, "paperback", "Z", "18", "Sisters Fiction, Romantic Comedy");
+INSERT INTO `books` VALUES (9780735219109, "Where the Crawdads Sing","Penguin Publishing Group","2021", "400", "English", null, "paperback", "E", "7", "Fiction");
+INSERT INTO `books` VALUES (9786094872082, "Priesaikos laužytojas", "BALTO leidybos namai", "2021", "320", "Lithuanian", 1, "hardcover", "E", "2", "Thriller, Suspense, Romantic");
 
 /*!40000 ALTER TABLE `books` ENABLE KEYS */;
 UNLOCK TABLES;
